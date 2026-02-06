6a52211d8b035edcd7808d565c42cbf82845f9f1
Kostiantyn <goncharov.kostiantyn@gmail.com>
2026-02-06 17:41:17 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,13 +1,42 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+
+    return {
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+
+    return {
+        "errors": [{
+            "line": error.get("line_number"),
+            "column": error.get("column_number"),
+            "message": error.get("text"),
+            "name": error.get("code"),
+            "source": "flake8"
+        } for error in errors],
+        "path": file_path,
+        "status": "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+
+    return [
+        {
+            "errors":
+                [{
+                    "line": error.get("line_number"),
+                    "column": error.get("column_number"),
+                    "message": error.get("text"),
+                    "name": error.get("code"),
+                    "source": "flake8"
+                } for error in report_items_value],
+            "path": report_items_key,
+            "status": "failed" if report_items_value else "passed"
+        } for report_items_key, report_items_value in linter_report.items()
+    ]
